cd9f3c339f0c5985464ac91e5ebc6b8c96c6fe38
mPeltzerNovi <m.peltzer@novi-education.nl>
2021-06-03 13:47:10 +0200

    Home added, Product started

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import './App.css';
 import Header from "./Header";
+import Home from "./Home";
 
 function App() {
   //BEM
   return (
     <div className="App">
       <Header />
-      {/* Home */}
+      <Home />
+
     </div>
   );
 }
