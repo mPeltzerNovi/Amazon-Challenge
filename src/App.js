0a2c93cb178794fa6fbb39b75767c25a79990d80
mPeltzerNovi <m.peltzer@novi-education.nl>
2021-06-04 13:36:14 +0200

    checkout added: React-Router-Dom, Switch

--- src/App.js
+++ src/App.js
@@ -2,15 +2,26 @@ import React from 'react';
 import './App.css';
 import Header from "./Header";
 import Home from "./Home";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   //BEM
   return (
-    <div className="App">
-      <Header />
-      <Home />
+      <Router>
+        <div className="App">
+            <Switch>
+                <Route path="/checkout">
+                    <Header />
+                    <h1>I am a checkout, smash the like button</h1>
+                </Route>
+                <Route path="/">
+                    <Header />
+                    <Home />
+                </Route>
 
-    </div>
+            </Switch>
+        </div>
+      </Router>
   );
 }
 
