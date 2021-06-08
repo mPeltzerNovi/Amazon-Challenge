cca612e245cdbe3567d245e7f1d3e105279d19ff
mPeltzerNovi <m.peltzer@novi-education.nl>
2021-06-08 17:02:34 +0200

    Checkout page build

--- src/Header.js
+++ src/Header.js
@@ -35,7 +35,7 @@ function Header() {
                 <Link to={!user && '/login'}> {/* if there was no user only than will we push to the login page*/}
                     <div onClick={handleAuthentication} className="header__option">
                         <span className='header__optionLineOne'>
-                            Hello Guest
+                            Hello {!user ? 'Guest' : user.email}
                         </span>
                         <span className='header__optionLineTwo'>
                             {user ? 'Sign Out' : 'Sign In'}
--- src/Home.js
+++ src/Home.js
@@ -16,7 +16,7 @@ function Home() {
                     <Product
                         id="12321341"
                         title="The Lean Startup: How Constant Innovation Creates Radically Successful Businesses Paperback"
-                        price={29.99}
+                        price={11.96}
                         image="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._SX325_BO1,204,203,200_.jpg"
                         rating={5}
 
