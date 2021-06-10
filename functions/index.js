cdaa4181249d6dd172d6f294e3d31fc0b73695b9
mPeltzerNovi <m.peltzer@novi-education.nl>
2021-06-10 14:57:43 +0200

    API build

---
+++ functions/index.js
@@ -0,0 +1,37 @@
+const functions = require("firebase-functions");
+const express = require("express");
+const cors = require("cors");
+const stripe = require("stripe")(
+    'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
+);
+
+
+// - API
+
+// - App config
+const app = express();
+
+// - Middlewarts
+app.use(cors({ origin: true }));
+app.use(express.json());
+
+// - API routes
+app.get('/', (request, response) => response.status(200).send('hello world'))
+
+app.post('/payments/create', async (request, response) => {
+    const total = request.query.total;
+
+    console.log('Payment Request Recieved BOOM!!! for this amount >>> ', total);
+
+    const paymentIntent = await stripe.paymentIntents.create({
+        amount: total,
+        currency: "usd",
+    });
+})
+
+// - Listen command
+exports.api = functions.https.onRequest(app)
+
+// Example endoint
+// http://localhost:5001/challenge-d2a70/us-central1/api
+
