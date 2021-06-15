408bf47ee89de36c779e8e5572f10cc5dc4a8060
mPeltzerNovi <m.peltzer@novi-education.nl>
2021-06-15 14:44:46 +0200

    Start Real Time Database build

--- functions/index.js
+++ functions/index.js
@@ -2,7 +2,7 @@ const functions = require("firebase-functions");
 const express = require("express");
 const cors = require("cors");
 const stripe = require("stripe")(
-    'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
+    'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
 );
 
 
--- src/Payment.js
+++ src/Payment.js
@@ -64,6 +64,10 @@ function Payment() {
             setError(null)
             setProcessing(false)
 
+            dispatch({
+                type: 'EMPTY_BASKET'
+            })
+
             history.replace('/orders')
 
         })
